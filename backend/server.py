bbfcd32aecb576238577dd0c3a62a72f5a70c721
sudharaD <72960779+sudharaD@users.noreply.github.com>
2024-01-14 10:22:35 +0530

    delete product function over
    linked with the frontend

--- backend/server.py
+++ backend/server.py
@@ -15,6 +15,14 @@ def get_products():
     return response
 
 
+@app.route("/deleteProduct", methods=["POST"])
+def delete_product():
+    return_id = products_dao.delete_product(connection, request.form["product_id"])
+    response = jsonify({"product_id": return_id})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
 if __name__ == "__main__":
     print("Start flask server for grocery store project")
     app.run(port=5000)
